1eb9891ab4d79bf28e0ffeda78389e21bb400456
Edelinefelicia <159420955+Edelinefelicia@users.noreply.github.com>
2024-06-27 08:28:25 +0700

    Add files via upload

---
+++ objects/OBJ_Bubble1.java
@@ -0,0 +1,33 @@
+package objects;
+
+import main.GamePanel;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+
+public class OBJ_Bubble1 extends SuperObject{
+    public GamePanel gp;
+
+    public OBJ_Bubble1(GamePanel gp) {
+        super(gp);
+        name= "BUbble";
+        try {
+            image = ImageIO.read(getClass().getResourceAsStream("/BG/bubblelobi1.png"));
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+//        collision = true;
+        this.gp = gp;
+
+        //ngegedein ukuran solid area mengikuti gambar
+        this.solidArea.width *= 2;
+        this.solidArea.height *= 2;
+    }
+
+    public void draw(Graphics2D g2, GamePanel gp){
+        //buat ngegedein ukuran kasur
+        g2.drawImage(image, worldX, worldY, gp.tileSize*2, gp.tileSize*2, null);
+    }
+}
